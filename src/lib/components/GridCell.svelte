537087054e513758e8ad71682853be70df96e343
programmingenjoyer219 <yuganshu219@gmail.com>
2025-05-03 15:00:49 +0530

    fix grid cell sizing issue by setting line height to 0 and using data attributes for variations

--- src/lib/components/GridCell.svelte
+++ src/lib/components/GridCell.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import type { LetterStatus } from '$lib/types';
-	import { gridCellDimensions } from '$lib/utils';
 	import type { Snippet } from 'svelte';
 
 	type Props = {
@@ -10,12 +9,12 @@
 	};
 
 	var { children, letterStatus, cellSize = 'sm' }: Props = $props();
-	const currentGridCellDimensions = gridCellDimensions[cellSize];
 </script>
 
 <div
+	data-cell-size={cellSize}
 	class={[
-		`grid place-content-center border-2 p-2 size-[${currentGridCellDimensions[0]}] sm:size-[${currentGridCellDimensions[0]}]`,
+		`grid place-content-center border-2 p-2`,
 		letterStatus == 'none' && 'border-gray-400',
 		letterStatus == 'correct' &&
 			'border-green-400 bg-green-400 dark:border-green-600 dark:bg-green-600',
@@ -26,10 +25,39 @@
 >
 	<span
 		class={[
-			'text-md font-bold sm:text-lg',
+			'text-md leading-0 font-bold sm:text-lg',
 			letterStatus != 'none' && 'text-white text-shadow-black/50 text-shadow-md'
 		]}
 	>
 		{@render children()}
 	</span>
 </div>
+
+<style>
+	[data-cell-size='sm'] {
+		height: 2rem;
+		width: 2rem;
+		@media (width >= 40rem) {
+			height: 2.5rem;
+			width: 2.5rem;
+		}
+	}
+
+	[data-cell-size='md'] {
+		height: 3.25rem;
+		width: 3.25rem;
+		@media (width >= 40rem) {
+			height: 3.75rem;
+			width: 3.75rem;
+		}
+	}
+
+	[data-cell-size='lg'] {
+		height: 4rem;
+		width: 4rem;
+		@media (width >= 40rem) {
+			height: 4.5rem;
+			width: 4.5rem;
+		}
+	}
+</style>
--- src/lib/features/word-grid/WordGrid.svelte
+++ src/lib/features/word-grid/WordGrid.svelte
@@ -1,17 +1,36 @@
 <script lang="ts">
 	import GridCell from '$lib/components/GridCell.svelte';
-	import { gridCellDimensions } from '$lib/utils';
 
-	const gridSize: 'sm' | 'md' | 'lg' = 'md';
-	const currentGridCellDimensions = gridCellDimensions[gridSize];
+	const gridColWidth: 'sm' | 'md' | 'lg' = 'md';
 </script>
 
 <section class="mb-2">
-	<div
-		class="grid grid-cols-[repeat(5,_{currentGridCellDimensions[0]})] place-content-center gap-1 sm:grid-cols-[repeat(5,_{currentGridCellDimensions[1]})]"
-	>
+	<div data-col-width={gridColWidth} class="grid place-content-center gap-1">
 		{#each { length: 30 }}
-			<GridCell cellSize={gridSize} letterStatus="none">{'A'}</GridCell>
+			<GridCell cellSize={gridColWidth} letterStatus="none">{'A'}</GridCell>
 		{/each}
 	</div>
 </section>
+
+<style>
+	[data-col-width='sm'] {
+		grid-template-columns: repeat(5, 2rem);
+		@media (width >= 40rem) {
+			grid-template-columns: repeat(5, 2.5rem);
+		}
+	}
+
+	[data-col-width='md'] {
+		grid-template-columns: repeat(5, 3.25rem);
+		@media (width >= 40rem) {
+			grid-template-columns: repeat(5, 3.75rem);
+		}
+	}
+
+	[data-col-width='lg'] {
+		grid-template-columns: repeat(5, 4rem);
+		@media (width >= 40rem) {
+			grid-template-columns: repeat(5, 4.5rem);
+		}
+	}
+</style>
