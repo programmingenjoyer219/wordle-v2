c0a2f9e7a263a69405b908876aabb9d9d57d9014
programmingenjoyer219 <yuganshu219@gmail.com>
2025-05-06 20:06:39 +0530

    statically generate the site

---
+++ src/routes/+error.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { page } from '$app/state';
+</script>
+
+<div class="flow">
+	<h1 class="text-heading-sm sm:text-heading-lg text-center">{page.status}</h1>
+	<p class="text-md text-center font-bold sm:text-lg">{page.error.message}</p>
+</div>
