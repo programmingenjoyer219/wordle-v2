0580197e57a9fe503f65b8afed7402e09722eede
programmingenjoyer219 <yuganshu219@gmail.com>
2025-05-08 22:13:08 +0530

    refactor to use Progress.svelte

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,12 +1,12 @@
 <script>
+	import Progress from '$lib/features/game/Progress.svelte';
 	import HowToPlay from '$lib/features/how-to-play/HowToPlay.svelte';
-	import PlayerProgress from '$lib/features/player-progress/PlayerProgress.svelte';
 	import AppSettings from '$lib/features/settings/AppSettings.svelte';
 </script>
 
 <header class="mb-2 border-b-2 border-gray-200/75 dark:border-gray-600/75">
 	<div class="flex items-center justify-between gap-2">
-		<PlayerProgress />
+		<Progress />
 		<div class="flex items-center gap-2">
 			<HowToPlay />
 			<AppSettings />
---
+++ src/lib/features/game/Progress.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	interface Props {
+		level?: number;
+		points?: number;
+	}
+
+	var { level = 1, points = 0 }: Props = $props();
+</script>
+
+<div class="flex items-center">
+	{@render ProgressInfo({ iconClass: 'ri-gamepad-line', info: `Lv-${level}` })}
+	{@render ProgressInfo({ iconClass: 'ri-focus-2-line', info: `${points}-pts` })}
+</div>
+
+{#snippet ProgressInfo({ iconClass, info }: { iconClass: string; info: string })}
+	<div class="flex items-center gap-1 px-3 py-1">
+		<i aria-hidden="true" class="{iconClass} ri-2x"></i>
+		<p class="text-sm font-bold">{info}</p>
+	</div>
+{/snippet}
